55b951337f4c1a8782f4125b66a4d731d7cbd205
par1tet <mankova486@gmail.com>
2024-07-25 19:55:46 +0300

    final layout

--- frontend-app/src/components/UI/inputs/MessageInput.jsx
+++ frontend-app/src/components/UI/inputs/MessageInput.jsx
@@ -2,5 +2,5 @@ import React from "react"
 import classes from './MessageInput.module.css'
 
 export const MessageInput = (props) => {
-    return (<input className={classes.MessageInput}></input>)
+    return (<input className={classes.MessageInput} placeholder="Введите сообщение..."></input>)
 }
